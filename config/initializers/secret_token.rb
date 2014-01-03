b1c2fc6b52726e6191ddd832c6f32b09087f62be
Matthew Star <mattjstar@gmail.com>
2014-01-02 20:48:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RawrScout::Application.config.secret_token = 'e2101de32dd224bdbf970957e9759da2b0c78894cd7e84e063a19d6971e71a4d6a34128a65f74d206a89b33c67183e772a478c595bf20011a91ecdc85b0468b7'
