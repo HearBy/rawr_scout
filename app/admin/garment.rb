bd445be4f3d2122ffaf742dd4cc3cb3fe73c88a4
Matthew Star <mattjstar@gmail.com>
2014-01-03 14:50:31 -0500

    hide garment and item admin models

--- app/admin/garment.rb
+++ app/admin/garment.rb
@@ -1,30 +1,30 @@
-ActiveAdmin.register Garment do
-  menu priority: 1
+# ActiveAdmin.register Garment do
+#   menu priority: 1
 
-  filter :brand, :as => :select
-  filter :name, :as => :select
-  filter :fabric_origin, :as => :select
-  filter :price, :as => :numeric
-  filter :denim_weight, :as => :numeric
-  filter :color, :as => :check_boxes, collection: Garment.all.map(&:color).uniq
-  filter :fit, :as => :check_boxes, collection: Garment.all.map(&:fit).uniq
+#   filter :brand, :as => :select
+#   filter :name, :as => :select
+#   filter :fabric_origin, :as => :select
+#   filter :price, :as => :numeric
+#   filter :denim_weight, :as => :numeric
+#   filter :color, :as => :check_boxes, collection: Garment.all.map(&:color).uniq
+#   filter :fit, :as => :check_boxes, collection: Garment.all.map(&:fit).uniq
   
-  index do
-    selectable_column
-    column :name, :sortable => :name do |garment|
-      link_to garment.name, garment.url, :target => "_blank"
-    end
-    column :brand
-    column :fabric_origin
-    column :made_in
-    column :price, :sortable => :price do |garment|
-      div :class => "price" do
-        number_to_currency garment.price
-      end
-    end
-    column :denim_weight
-    column :color
-    column :fit
-    default_actions
-  end
-end
+#   index do
+#     selectable_column
+#     column :name, :sortable => :name do |garment|
+#       link_to garment.name, garment.url, :target => "_blank"
+#     end
+#     column :brand
+#     column :fabric_origin
+#     column :made_in
+#     column :price, :sortable => :price do |garment|
+#       div :class => "price" do
+#         number_to_currency garment.price
+#       end
+#     end
+#     column :denim_weight
+#     column :color
+#     column :fit
+#     default_actions
+#   end
+# end
