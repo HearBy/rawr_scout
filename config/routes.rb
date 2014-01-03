d23fc1d6b7333dfc934fa47b9dff557861131323
Matthew Star <mattjstar@gmail.com>
2014-01-02 21:13:40 -0500

    fix root to for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RawrScout::Application.routes.draw do
-  root 'static_pages#home'
+  root to: 'static_pages#home'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
