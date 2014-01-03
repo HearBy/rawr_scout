d39a6b955ba0a6c3620ca93edbbb8c6dab2217d2
Matthew Star <mattjstar@gmail.com>
2014-01-03 16:57:09 -0500

    add tests for admin_user model

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe AdminUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+	before do 
+		@admin_user = create(:admin_user)
+	end
+
+	subject { @admin_user }
+
+	it { should respond_to(:email) }
+	it { should respond_to(:role) }
+
+	it { should have_many(:items) }
+
+	describe "admin must be one of two options" do
+		before { @admin_user.role = "test" }
+		
+		it { should_not be_valid }
+
+		describe "admin" do
+			describe "valid" do
+			    before { @admin_user.role = "admin" }
+			    it { should be_valid }
+			end
+
+			describe "not valid" do
+			    before { @admin_user.role = "Admin" }
+			    it { should_not be_valid }
+			end
+		end
+
+		describe "item_only" do
+			describe "valid" do
+			    before { @admin_user.role = "item_only" }
+			    it { should be_valid }
+			end
+
+			describe "not valid" do
+			    before { @admin_user.role = "Item_only" }
+			    it { should_not be_valid }
+			end
+		end
+	end
 end
